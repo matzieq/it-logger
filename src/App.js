44f7cfba9dbbfe909eb4a5252edf29eeccbad594
matzieq <mjp.kozlowski@gmail.com>
2019-12-29 11:51:54 +0100

    Add log components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from "react";
-import SearchBar from "./components/layout/SearchBar";
+import SearchBar from "components/layout/SearchBar";
+import Logs from "components/logs/Logs";
 import "materialize-css/dist/css/materialize.min.css";
 import M from "materialize-css/dist/js/materialize.min.js";
 import "./App.css";
@@ -11,6 +12,9 @@ const App = () => {
   return (
     <>
       <SearchBar />
+      <div className="container">
+        <Logs />
+      </div>
     </>
   );
 };
--- src/components/logs/LogItem.js
+++ src/components/logs/LogItem.js
@@ -1,7 +1,25 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
+import PropTypes from "prop-types";
 
-const LogItem = () => {
-  return <div></div>;
+const LogItem = ({ log }) => {
+  return (
+    <li className="collection-item">
+      <div className="">
+        <a
+          href="#edit-log-modal"
+          className={`modal-trigger ${
+            log.attention ? "red-text" : "blue-text"
+          }`}
+        >
+          {log.message}
+        </a>
+      </div>
+    </li>
+  );
+};
+
+LogItem.propTypes = {
+  log: PropTypes.object.isRequired
 };
 
 export default LogItem;
