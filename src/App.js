2762b9c34226aefd7b279426a39ce24de3800d49
Maciej Kozłowski <mjp.kozlowski@gmail.com>
2019-12-30 15:28:49 +0100

    Add redux

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ import AddLogModal from "components/logs/AddLogModal";
 import AddTechModal from "components/techs/AddTechModal";
 import EditLogModal from "components/logs/EditLogModal";
 import TechListModal from "components/techs/TechListModal";
+import { Provider } from "react-redux";
+import store from "./store";
+
 import "materialize-css/dist/css/materialize.min.css";
 import M from "materialize-css/dist/js/materialize.min.js";
 import "./App.css";
@@ -15,7 +18,7 @@ const App = () => {
     M.AutoInit();
   });
   return (
-    <>
+    <Provider store={store}>
       <SearchBar />
       <div className="container">
         <AddBtn />
@@ -25,7 +28,7 @@ const App = () => {
         <EditLogModal />
         <Logs />
       </div>
-    </>
+    </Provider>
   );
 };
 
